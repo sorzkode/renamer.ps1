908be7e1ad77b5e0daad86058c64bb3d158b4d10
RILEYJITSU <rileyjitsu@gmail.com>
2023-06-09 14:45:42 -0400

    general updates

--- README.md
+++ README.md
@@ -1,15 +1,25 @@
 # renamer.ps1
-Generic PowerShell script to bulk rename files based on user input. 
+
+PowerShell script to bulk rename files based on user input.
 
 ![alt text](https://github.com/sorzkode/renamer.ps1/blob/main/reNAME.png?raw=true)
 
-1. Place the renamer.ps1 script in the folder where the files are located that you want to rename.
-2. Open PowerShell and change directories to where the folder is located (IE cd "C:\users\user\New folder"). 
-3. Type **./renamer.ps1** to run the script.
-    
-    -**Please note:** *You may need to change your execution policy to "unrestricted" in order to run scripts. This is disabled by default for security purposes. To do this you would type* **Set-ExecutionPolicy Unrestricted** *and follow the prompts.* 
-4. Enter the text you want to replace when prompted by the script. 
-5. Enter the new text you wanted added to the files when prompted by the script. 
+## Instructions
+```
+    1. Place the `renamer.ps1` script in the folder where the files are located that you want to rename.
 
-**If you are uncomfortable running a script or just want to do it manually; You can open PowerShell and type the following command:** 
+    2. Open PowerShell and change directories to where the folder is located (e.g., `cd "C:\users\user\New folder"`).
+
+    3. Run the script by typing `./renamer.ps1` in the PowerShell prompt.
+
+    - **Please note:** You may need to change your execution policy to "Unrestricted" in order to run scripts. By default, the execution policy is disabled for security purposes. To change the execution policy, use the command `Set-ExecutionPolicy Unrestricted` and follow the prompts.
+
+    4. When prompted by the script, enter the text you want to replace.
+
+    5. When prompted by the script, enter the new text you want to add to the files.
+```
+
+**If you are uncomfortable running a script or prefer to do it manually, you can open PowerShell and use the following command:**
+```
 > ls | Rename-Item -NewName {$_.name -replace "curent text to be replaced","new text"}
+```
--- renamer.ps1
+++ renamer.ps1
@@ -1,3 +1,22 @@
+'''
++-+-+-+-+-+-+
+|r|e|N|A|M|E|
++-+-+-+-+-+-+
+                                                   
+PowerShell File Renamer Script.
+-
+Author:
+Mister Riley
+sorzkode@proton.me
+https://github.com/sorzkode
+
+MIT License
+Copyright (c) 2023 Mister Riley
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+'''
+
 Write-Host " __      __         .__                                    
 /  \    /  \  ____  |  |    ____   ____    _____    ____   
 \   \/\/   /_/ __ \ |  |  _/ ___\ /  _ \  /     \ _/ __ \  
@@ -5,20 +24,50 @@ Write-Host " __      __         .__
   \__/\  /   \___  >|____/ \___  >\____/ |__|_|  / \___  > 
        \/        \/            \/              \/      \/  
                                                            "
-Write-Host "reNAME script by sorzkode"
+
+Write-Host "reNAME script by Mister Riley"
 Write-Host "We will need a few things to get started..."
+
+# Prompt for the file name to replace
 $oldname = Read-Host -Prompt 'File name you want to replace'
+
+# Prompt for the new file name
 $newname = Read-Host -Prompt 'New file name'
-Write-Host "Make sure the renamer.ps1 script is in the same folder as the files to be renamed and that you have changed directories (cd) within PowerShell to this folder."
 
- ls | Rename-Item -NewName {$_.name -replace $oldname,$newname}
+# Prompt for the location to search or specify to search the whole PC
+$searchLocation = Read-Host -Prompt 'Enter a specific location to search (leave empty to search the whole PC)'
 
-Write-Host "___________.__         .__         .__                 .___._. 
-\_   _____/|__|  ____  |__|  ______|  |__    ____    __| _/| | 
- |    __)  |  | /    \ |  | /  ___/|  |  \ _/ __ \  / __ | | | 
- |     \   |  ||   |  \|  | \___ \ |   Y  \\  ___/ / /_/ |  \| 
- \___  /   |__||___|  /|__|/____  >|___|  / \___  >\____ |  __ 
-     \/             \/          \/      \/      \/      \/  \/ 
-                                                               "
+# Store the results of the search
+if ([string]::IsNullOrWhiteSpace($searchLocation)) {
+    $files = Get-ChildItem -Recurse -File
+} else {
+    $files = Get-ChildItem -Recurse -File -Path $searchLocation
+}
+
+# Check if the new file name already exists
+$existingFiles = $files | Where-Object { $_.Name -eq $newname }
+
+if ($existingFiles.Count -gt 0) {
+    Write-Host "The new file name '$newname' already exists in the specified location(s)."
+    $proceed = Read-Host -Prompt "Do you want to proceed? (Y/N)"
+
+    if ($proceed -ne "Y" -and $proceed -ne "y") {
+        Write-Host "Operation canceled by the user."
+        Exit
+    }
+}
+
+# Rename the files and handle any errors
+try {
+    $files | ForEach-Object {
+        $newNameWithPath = Join-Path -Path $_.DirectoryName -ChildPath ($_.Name -replace $oldname, $newname)
+        Rename-Item -Path $_.FullName -NewName $newNameWithPath -ErrorAction Stop
+    }
+
+    Write-Host "Finished"
+}
+catch {
+    Write-Host "An error occurred while renaming the files: $_"
+}
 
 Exit
